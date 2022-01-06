d2fe0116ff4674282d52458d29d335833523bb05
skumar <ch@ng3m3>
2022-01-06 15:47:23 -0600

    Json-server example with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'json-server';
+  posts: any = [];
+
+  constructor(private service: AppService) {
+  }
+
+  ngOnInit() {
+    this.service.get().subscribe(data => this.posts = data);
+  }
 }
